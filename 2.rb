679e1b270230ac9e1613d4e38f89f8bd3e1876c5
hitode909 <hitode909@gmail.com>
2013-04-29 18:53:55 +0900

    簡単に

--- 2.rb
+++ 2.rb
@@ -6,20 +6,21 @@ REPOSITORY_PATH = ARGV.first or raise "repository_path required"
 
 BRANCH = ARGV[1] || 'master'
 
-class Yousu
-    def self.each_commits(repository, branch = 'master', index = 0, &block)
-        max_count = 10
+def all_commits(repository, branch = 'master')
+    index = 0
+    max_count = 10
+    commits = [true]
+    while commits.any?
         commits = repository.commits(BRANCH, max_count, index)
-        return unless commits.length > 0
         commits.each{ |commit|
             yield commit
         }
-        each_commits(repository, branch, index + max_count, &block)
+        index += max_count
     end
 end
 
 repository = Grit::Repo.new(REPOSITORY_PATH)
-Yousu.each_commits(repository, BRANCH) { |commit|
+all_commits(repository, BRANCH) { |commit|
     commit.stats.to_diffstat.each{ |stat|
         next unless stat.filename =~ /^lib/
         blob = commit.tree / stat.filename
